81309ad9b9c584be2c0e5f1236cdea2ae99f5e44
ansaier <3117254928@qq.com>
2025-11-13 21:08:55 +0800

    bugfix

--- pkg/aaa_steam_minshe/skills/beishui.lua
+++ pkg/aaa_steam_minshe/skills/beishui.lua
@@ -4,7 +4,7 @@ local beishui = fk.CreateSkill{
 
 Fk:loadTranslationTable{
     ["steamMinshe__beishui"] = "背水",
-    [":steamMinshe__beishui"] = "每个回合结束时，若你手牌数不大于1，你执行一个视为拥有“<a href = ':steamMinshe__beishui_qingjiao'><font color='#195494'>清剿</font></a>”且“膂力”中的“或”改成“和”的出牌阶段。",
+    [":steamMinshe__beishui"] = "每个回合结束时，若你手牌数不大于1，你执行一个视为拥有且仅有“<a href = ':steamMinshe__beishui_qingjiao'><font color='#195494'>清剿</font></a>”且“膂力”中的“或”改成“和”的出牌阶段的回合。",
     ["@[desc]steamMinshe__beishui-phase"] = "背水",
     [":steamMinshe__beishui_qingjiao"] = "<b>清剿</b>：出牌阶段开始时，你可以弃置所有手牌，然后从牌堆或弃牌堆中随机获得八张牌名各不相同且副类别不同的牌。若如此做，结束阶段，你弃置所有牌。",
     [":steamMinshe__beishui-phase"] = "效淮阴之举，力敌数千！<br>你视为拥有“清剿”且“膂力”中的“或”改成“和”",
@@ -21,7 +21,8 @@ beishui:addEffect(fk.TurnEnd, {
     on_use = function (self, event, target, player, data)
         local room = player.room
         room:setPlayerMark(player, "@[desc]steamMinshe__beishui-phase", 1)
-        player:gainAnExtraPhase(Player.Play, beishui.name)
+        -- player:gainAnExtraPhase(Player.Play, beishui.name)
+        player:gainAnExtraTurn(true, beishui.name, { Player.Play })
     end,
 })
 
